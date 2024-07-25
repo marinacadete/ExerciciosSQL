c3bf4247cbb05330827f3a1cae45d6d51feb17ec
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2024-07-25 10:59:44 -0300

    Update Inner Join

--- Codigos/InnerJoin.sql
+++ Codigos/InnerJoin.sql
@@ -3,3 +3,9 @@ FROM categories
 INNER JOIN products
 ON categories.id = products.id_categories
 GROUP BY categories.name;
+
+SELECT movies.id, name
+FROM movies
+INNER JOIN genres
+ON movies.id_genres = genres.id
+WHERE description = 'Action';
