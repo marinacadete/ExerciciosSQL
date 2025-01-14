10f2cbe073cb38a01bcf8f8088c39cac7e00d93c
Marina Cadete <147002557+marinacadete@users.noreply.github.com>
2025-01-14 10:38:37 -0300

    Update Exer2.sql

--- Codigos/Exer2.sql
+++ Codigos/Exer2.sql
@@ -3,3 +3,10 @@ FROM customers AS c
 FULL OUTER JOIN orders AS o
 ON c.id = o.id_customers
 WHERE orders_date BETWEEN '2016-01-01' AND '2016-06-30';
+
+
+SELECT products.name
+FROM providers
+FULL OUTER JOIN products
+ON providers.id = products.id_providers
+WHERE products.amount BETWEEN 10 AND 20 AND providers.name LIKE 'P%';
